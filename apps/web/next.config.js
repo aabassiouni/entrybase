d2162bd3624f013c8eec3a496334cc153febfe0e
Ali Bassiouni <aabassiouni@hotmail.com>
2023-11-02 15:04:04 +0200

    update metadata for next14

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        optimizePackageImports: ['@radix-ui/react-icons']
+        optimizePackageImports: ['@radix-ui/react-icons'],
+        // serverActions: true,
     }
 }
 
