63e2550ab63e713ccf21632afa7b13395466faf2
Ali Bassiouni <aabassiouni@hotmail.com>
2023-11-04 06:05:03 +0200

    Update page.tsx

--- apps/web/src/app/invite/page.tsx
+++ apps/web/src/app/invite/page.tsx
@@ -1,44 +1,50 @@
 "use client";
-
+import { useInvites } from "@/components/context/invite-context";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import Link from "next/link";
 import React, { useState } from "react";
-import { Separator } from "@/components/ui/separator";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import EmailPreview from "@/components/email-preview";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 function InvitePage() {
-	const [email, setEmail] = useState("");
-	const [headerSectionColor, setHeaderSectionColor] = useState("bg-gray-900");
+	const { invites } = useInvites();
+	const [isLoading, setIsLoading] = useState(false);
+	console.log(invites);
 
-	function handleInputChange(event: any) {
-		setEmail(event.target.value);
+	async function handleSendInvites() {
+		console.log("send invites");
+		setIsLoading(true);
+		const data = await fetch("/api/send", {
+			method: "POST",
+			body: JSON.stringify({ invites }),
+		});
+		const res = await data.json();
+		console.log(res);
+		setIsLoading(false);
 	}
 	return (
-		<div className="flex w-full">
-			<div className="w-1/2 p-8">
-				<h1 className="text-3xl">Edit Invite Email</h1>
-				<Separator className="my-4" />
-				<Label htmlFor="email">Subject</Label>
-				<Input id="email" onChange={handleInputChange} className="my-1 w-1/4" />
-				<Select onValueChange={setHeaderSectionColor}>
-					<SelectTrigger className="w-fit">
-						<SelectValue placeholder="Header Section Color" />
-					</SelectTrigger>
-					<SelectContent>
-						<SelectItem value="bg-blue-800">blue</SelectItem>
-						<SelectItem value="pastDay">Option 2</SelectItem>
-						<SelectItem value="pastMonth">Option 3</SelectItem>
-					</SelectContent>
-				</Select>
-			</div>
-			<div className="w-1/2">
-				<div>
-					<h1 className="py-8 text-center text-3xl">Invite Preview</h1>
-				</div>
-
-				<EmailPreview email={email} headerSectionColor={headerSectionColor} />
-			</div>
+		<div className="flex w-full items-center justify-center">
+			<Card className="w-full max-w-lg">
+				<CardHeader>
+					<CardTitle>Confirm Your Invitations</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<div className="pb-4">
+						<Link href={`/email-preview`}>Preview email</Link>
+					</div>
+					<div>
+						<h1>Iniviting:</h1>
+						<ScrollArea className="h-24">
+							{invites.length > 0 ? invites.map((invite) => <p>{invite}</p>) : <p>No invites</p>}
+						</ScrollArea>
+					</div>
+				</CardContent>
+				<CardFooter className="justify-end">
+					<Button onClick={handleSendInvites} disabled={invites.length === 0}>
+						{isLoading ? "Sending..." : "Send"}
+					</Button>
+				</CardFooter>
+			</Card>
 		</div>
 	);
 }
