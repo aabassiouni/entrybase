593ed06b3fea413d7af62adc094d9ca99f6bbad3
Ali Bassiouni <aabassiouni@hotmail.com>
2023-11-12 07:37:52 +0200

    change chart color to green

--- apps/web/src/components/Chart.tsx
+++ apps/web/src/components/Chart.tsx
@@ -37,8 +37,8 @@ function Chart({ data, margin }: { data: any; margin: Margin }) {
 			<AreaChart data={data} margin={margin}>
 				<defs>
 					<linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-						<stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-						<stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+						<stop offset="5%" stopColor="#4BE7AE" stopOpacity={0.8} />
+						<stop offset="95%" stopColor="#4BE7AE" stopOpacity={0} />
 					</linearGradient>
 				</defs>
 				<XAxis tickLine={false} dataKey="label">
@@ -53,7 +53,7 @@ function Chart({ data, margin }: { data: any; margin: Margin }) {
 				<Tooltip content={<CustomTooltip />} />
 				<Legend formatter={renderLegend} />
 				<CartesianGrid vertical={false}  strokeOpacity={0.1} strokeDasharray={"3 3"}  />
-				<Area dot={true} type={"monotone"}   dataKey="value" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+				<Area dot={true} type={"monotone"}   dataKey="value" stroke="#AFF4DB" fillOpacity={1} fill="url(#colorUv)" />
 			</AreaChart>
 		</ResponsiveContainer>
 	);
