ccb97cf8a5c5ec899e51c22a1449f3aebbc5ff20
Ali Bassiouni <aabassiouni@hotmail.com>
2023-11-24 08:34:39 +0200

    add not found handling to dashboard pages

---
+++ apps/web/src/app/(authenticated)/dashboard/[waitlist]/[...notfound]/page.tsx
@@ -0,0 +1,8 @@
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+function NotFound({params}: {params: {waitlist: string}}) {
+  return redirect(`/dashboard/${params.waitlist}`)
+}
+
+export default NotFound
