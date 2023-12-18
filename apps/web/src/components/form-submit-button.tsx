47bebd11706a9189b6ecdedde626828cfbce894f
Ali Bassiouni <aabassiouni@hotmail.com>
2023-12-18 05:55:27 +0200

    fix: make loading optional in form submit btn

--- apps/web/src/components/form-submit-button.tsx
+++ apps/web/src/components/form-submit-button.tsx
@@ -5,7 +5,7 @@ import { Button } from "./ui/button";
 import { Loader2 } from "lucide-react";
 import { cn } from "@/lib/utils";
 
-function FormSubmitButton({loading, children, className }: {loading:boolean, children: React.ReactNode; className?: string }) {
+function FormSubmitButton({loading, children, className }: {loading?:boolean, children: React.ReactNode; className?: string }) {
 	const { pending, action,data,method } = useFormStatus();
 	// console.log({pending, action,data,method})
 	return (
