89144ebd6433e45cbdcacc0c56a9dee1012b9241
Ali Bassiouni <aabassiouni@hotmail.com>
2023-12-30 07:08:18 +0200

    fix: use form submit btn in create wt dialog

--- apps/web/src/components/create-waitlist-dialog.tsx
+++ apps/web/src/components/create-waitlist-dialog.tsx
@@ -1,10 +1,9 @@
 import { createWaitlistAction } from "@/lib/actions";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
-import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import FormSubmitButton from "./form-submit-button";
 
 function CreateWaitlistDialog({ children, userID }: { children: React.ReactNode; userID: string }) {
-
 	return (
 		<Dialog>
 			<DialogTrigger asChild>{children}</DialogTrigger>
@@ -15,7 +14,7 @@ function CreateWaitlistDialog({ children, userID }: { children: React.ReactNode;
 				</DialogHeader>
 				<form action={createWaitlistAction} className="flex flex-col space-y-4">
 					<Input name="waitlistName" />
-					<Button className="">Create</Button>
+					<FormSubmitButton>Create</FormSubmitButton>
 				</form>
 			</DialogContent>
 		</Dialog>
