1f280915fed822e4e762725539053c944f2bbf65
Ali Bassiouni <aabassiouni@hotmail.com>
2024-01-01 11:49:54 +0200

    change color on github btn in landing

--- apps/web/src/app/(landing)/page.tsx
+++ apps/web/src/app/(landing)/page.tsx
@@ -246,7 +246,7 @@ function LandingPage() {
 								<GitHubLogoIcon className="h-6 w-6 text-white" />
 								<p className="font-semibold text-white">Star On GitHub</p>
 							</div> */}
-							<button className="animate-background-shine inline-flex h-12 items-center justify-center gap-2 rounded-md border border-primary bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+							<button className="animate-background-shine inline-flex h-12 items-center justify-center gap-2 rounded-md border border-primary bg-[linear-gradient(110deg,#000103,45%,#4BE7AE,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors">
 								<GitHubLogoIcon className="h-6 w-6 text-white" />
 								<p className="font-semibold text-white">Star On GitHub</p>
 							</button>
