2ef704b4818ba22c5db862942bf3eb19eb239122
Ali Bassiouni <aabassiouni@hotmail.com>
2024-01-28 23:58:05 +0200

    add resend webhook

---
+++ apps/web/src/app/api/webhook/resend/route.ts
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+	console.log("resend webhook received");
+	const body = await request.json();
+
+	const { type, created_at, data } = body;
+
+	switch (type) {
+        case "email.sent":
+            console.log("email.sent");
+
+            return NextResponse.json({ status: "ok" });
+		case "email.opened":
+			console.log("email.opened");
+
+			return NextResponse.json({ status: "ok" });
+		case "email.delivered":
+			console.log("email.delivered");
+
+			return NextResponse.json({ status: "ok" });
+		case "email.complained":
+			console.log("email.failed");
+
+			return NextResponse.json({ status: "ok" });
+		case "email.clicked":
+			console.log("email.clicked");
+
+			return NextResponse.json({ status: "ok" });
+		case "email.delivery_delayed":
+			console.log("email.delivery_delayed");
+
+			return NextResponse.json({ status: "ok" });
+		case "email.bounced":
+			console.log("email.bounced");
+
+			return NextResponse.json({ status: "ok" });
+	}
+}
