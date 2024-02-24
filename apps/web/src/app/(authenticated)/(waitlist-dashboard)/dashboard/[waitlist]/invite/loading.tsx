e8579e69c9586ef36f1f207928ad34b6f426c9f4
Ali Bassiouni <aabassiouni@hotmail.com>
2024-02-24 23:49:35 +0200

    add loading page to invite page

---
+++ apps/web/src/app/(authenticated)/(waitlist-dashboard)/dashboard/[waitlist]/invite/loading.tsx
@@ -0,0 +1,17 @@
+import { MainLayout } from "@/components/layout";
+import { PageHeading } from "@/components/typography";
+import { Skeleton } from "@/components/ui/skeleton";
+import React from "react";
+
+function InvitePageLoading() {
+	return (
+		<MainLayout>
+			<PageHeading>Invite</PageHeading>
+			<div className="flex h-full w-full items-center justify-center">
+				<Skeleton className="h-full w-full max-w-lg" />
+			</div>
+		</MainLayout>
+	);
+}
+
+export default InvitePageLoading;
