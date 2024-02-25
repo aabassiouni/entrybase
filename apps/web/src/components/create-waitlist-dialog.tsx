7dec7cfe22cfabfb77408ec4b91bfc138a8d4771
Ali Bassiouni <aabassiouni@hotmail.com>
2024-02-26 01:14:08 +0200

    testing

--- apps/web/src/components/create-waitlist-dialog.tsx
+++ apps/web/src/components/create-waitlist-dialog.tsx
@@ -1,9 +1,32 @@
-import { createWaitlistAction } from "@/lib/actions";
+// import { createWaitlistAction } from "@/lib/actions";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
 import { Input } from "./ui/input";
 import FormSubmitButton from "./form-submit-button";
+import { createWaitlist, initEmailTemplates } from "@/lib/db";
+import { notifyDiscord } from "@/lib/discord";
+import { notFound, redirect } from "next/navigation";
+import { checkWorkspace } from "@/lib/auth";
 
 function CreateWaitlistDialog({ children }: { children: React.ReactNode }) {
+
+	 async function createWaitlistAction(formData: FormData) {
+		'use server'
+		// const { userId } = auth();
+		const workspace = await checkWorkspace();
+		if (!workspace) {
+			return notFound();
+		}
+	
+		const waitlistName = formData.get("waitlistName") as string;
+	
+		const [{ waitlistID }] = await createWaitlist(waitlistName, workspace.workspaceID);
+	
+		await initEmailTemplates(waitlistID);
+		notifyDiscord({
+			waitlist: waitlistID,
+		});
+		redirect(`/dashboard/${waitlistID}`);
+	}
 	return (
 		<Dialog>
 			<DialogTrigger asChild>{children}</DialogTrigger>
