b8e44ec9ee07f0470fb9bff3001f6972b8f58ab0
Ali Bassiouni <aabassiouni@hotmail.com>
2024-03-04 02:56:24 +0200

    fix tailwind styles being applied twice

--- apps/web/src/app/(authenticated)/layout.tsx
+++ apps/web/src/app/(authenticated)/layout.tsx
@@ -1,5 +1,5 @@
 import "../globals.css";
-import "@uploadthing/react/styles.css";
+// import "@uploadthing/react/styles.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
