05d6de3538200e62ce7f617ceeadf9fcb595cced
Ali Bassiouni <aabassiouni@hotmail.com>
2024-03-08 21:09:59 +0200

    update new workspace page

--- apps/web/src/app/(authenticated)/(home-page)/dashboard/new/page.tsx
+++ apps/web/src/app/(authenticated)/(home-page)/dashboard/new/page.tsx
@@ -1,10 +1,12 @@
 import { PageHeading } from "@/components/typography";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
 
 import { currentUser } from "@clerk/nextjs";
+import { Label } from "@/components/ui/label";
+import { Info } from "lucide-react";
 
 async function NewWorkspacePage() {
 	const user = await currentUser();
@@ -18,15 +20,22 @@ async function NewWorkspacePage() {
 			<Card className="flex flex-col">
 				<form method="POST" action="/api/checkout">
 					<CardHeader>
-						<CardTitle>Workspace Name</CardTitle>
-						<CardDescription>Enter a name for your workspace</CardDescription>
+						<CardTitle>Create Pro workspace</CardTitle>
+						<div>
+							<p className="text-neutral-500">Get access to all the features</p>
+						</div>
 					</CardHeader>
 					<CardContent className="">
-						<Input name="workspaceName" required className="w-42" placeholder="Workspace Name" />
+						<Label>Workspace Name</Label>
+						<Input name="workspaceName" required className="w-42" placeholder="Pro workspace" />
 					</CardContent>
 					<Separator />
-					<CardFooter className="pt-6">
-						<Button className="w-fit">Create Workspace</Button>
+					<CardFooter className="pt-6 gap-4">
+						<Button className="w-fit">Checkout</Button>
+						<div className="flex items-center gap-2 text-neutral-500">
+							<Info size={16}/>
+							<p className="text-sm">You will be taken to a Stripe checkout page</p>
+						</div>
 					</CardFooter>
 				</form>
 			</Card>
