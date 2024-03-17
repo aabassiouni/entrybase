3b0706c03a02a7bd0e867c6606bb8e62cbacb739
Ali Bassiouni <aabassiouni@hotmail.com>
2024-03-17 04:45:39 +0200

    add api bento img

--- apps/web/src/app/(landing)/page.tsx
+++ apps/web/src/app/(landing)/page.tsx
@@ -10,6 +10,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import homepageImg from "/public/homepage.png";
+import apiBentoImg from "./apiBentoImg.png";
 
 function LandingPage() {
 	return (
@@ -203,6 +204,10 @@ function LandingPage() {
 						</div>
 					</GradientBorderCard>
 					<GradientBorderCard flexdir="flex-col-reverse" className="sm:col-span-3 sm:row-span-2">
+						<div className="flex h-full items-center justify-center bg-rd-600">
+							<div className="absolute top-0 right-0 bottom-0 left-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]" />
+							<Image className="z-10" src={apiBentoImg} width={400} alt="api" />
+						</div>
 						<div>
 							<GradientBorderCardTitle>Seamless API Integration</GradientBorderCardTitle>
 							<GradientBorderCardDescription>
