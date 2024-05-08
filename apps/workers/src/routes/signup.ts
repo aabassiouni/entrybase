289ca3a8575f650f60a982eafc5ddf0b99d98ce5
Ali Bassiouni <aabassiouni@hotmail.com>
2024-05-08 16:32:36 +0300

    increment from worker

--- apps/workers/src/routes/signup.ts
+++ apps/workers/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import { signups } from "@entrybase/db";
 import { newId } from "@entrybase/id";
 import { sql } from "drizzle-orm";
-import { Context } from "hono";
+import type { Context } from "hono";
 import { db } from "../db";
 
 export async function signupRoute(c: Context) {
@@ -11,14 +11,25 @@ export async function signupRoute(c: Context) {
 		const email = body.email;
 
 		const signupID = newId("su");
-		const record = await db.insert(signups).values({
+		const _record = await db.insert(signups).values({
 			signupID,
 			waitlistID: waitlist,
 			email,
 		});
 
+		await fetch("http://localhost:9999/receive", {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				waitlist_id: waitlist,
+				update: "update from workers",
+			}),
+		})
+
 		return c.json({ id: signupID, message: "Signup Successful" });
-	} catch (e) {
+	} catch {
 		return c.json({ message: "Internal Server Error :(" }, { status: 500 });
 	}
 }
