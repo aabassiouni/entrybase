067bd73000d0591f2c67a85580a41ffb47aa92fe
Ali Bassiouni <aabassiouni@hotmail.com>
2024-05-08 15:59:27 +0300

    delete old go api

--- apps/api/pkg/database/queries/insert_signup.sql
+++
@@ -1,8 +0,0 @@
--- name: InsertSignupQuery :exec
-
-INSERT INTO signups (waitlist_id, email, first_name, last_name)
-VALUES (@waitlist_id,
-        @email,
-        @first_name,
-        @last_name) ;
-
