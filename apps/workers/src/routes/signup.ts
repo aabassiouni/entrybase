5159fed426572cf55c4e0e8a01d7faef32dca20b
Ali Bassiouni <aabassiouni@hotmail.com>
2024-05-21 22:19:47 +0300

    log errors in workers

--- apps/workers/src/routes/signup.ts
+++ apps/workers/src/routes/signup.ts
@@ -5,31 +5,36 @@ import { env } from "hono/adapter";
 import { db } from "../db";
 
 export async function signupRoute(c: Context) {
-	try {
-		const waitlist = c.req.param("waitlist");
-		const body = await c.req.json();
-		const email = body.email;
+	const waitlist = c.req.param("waitlist");
+	const body = await c.req.json();
+	const email = body.email;
 
-		const signupID = newId("su");
-		const _record = await db.insert(signups).values({
+	const signupID = newId("su");
+
+	const _record = await db
+		.insert(signups)
+		.values({
 			signupID,
 			waitlistID: waitlist,
 			email,
+		})
+		.catch((e) => {
+			console.log(e);
+			return c.json({ message: "Internal Server Error" }, { status: 500 });
 		});
 
-		await fetch(`${env(c).REALTIME_API_URL}/receive`, {
-			method: "PUT",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				waitlist_id: waitlist,
-				update: "update from workers",
-			}),
-		});
+	await fetch(`${env(c).REALTIME_API_URL}/receive`, {
+		method: "PUT",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			waitlist_id: waitlist,
+			update: "update from workers",
+		}),
+	}).catch((e) => {
+		console.log(e);
+	});
 
-		return c.json({ id: signupID, message: "Signup Successful" });
-	} catch {
-		return c.json({ message: "Internal Server Error" }, { status: 500 });
-	}
+	return c.json({ id: signupID, message: "Signup Successful" });
 }
