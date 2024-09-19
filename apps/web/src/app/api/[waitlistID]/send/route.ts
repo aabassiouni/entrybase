0263ef0ca48acc1d580f72d144ec106600ca5617
Ali Bassiouni <aabassiouni@hotmail.com>
2024-09-19 06:16:55 +0300

    refactor send route

--- apps/web/src/app/api/[waitlistID]/send/route.ts
+++ apps/web/src/app/api/[waitlistID]/send/route.ts
@@ -16,7 +16,20 @@ import { Resend } from "resend";
 const resend = new Resend(env().RESEND_API_KEY);
 
 export async function POST(request: NextRequest, context: { params: { waitlistID: string } }) {
-  console.log("sending emails");
+  const type = request.nextUrl.searchParams.get("type");
+  const waitlistID = context.params.waitlistID;
+
+  // get user id from clerk
+  const { userId } = auth();
+  if (!userId) {
+    return NextResponse.redirect("/login");
+  }
+
+  // check if user has waitlist
+  const workspace = await checkWorkspace(waitlistID);
+  if (!workspace) {
+    return NextResponse.redirect("/dashboard");
+  }
 
   const data = (await request.json()) as {
     selectionMethod: string;
@@ -26,28 +39,11 @@ export async function POST(request: NextRequest, context: { params: { waitlistID
 
   const { selectionMethod, inviteCount, invitesList } = data;
 
-  // get user id from clerk
-  const { userId } = auth();
-
-  if (!userId) {
-    return NextResponse.redirect("/login");
-  }
-
-  const type = request.nextUrl.searchParams.get("type");
-
-  const waitlistID = context.params.waitlistID;
-
-  // check if user has waitlist
-  const workspace = await checkWorkspace(waitlistID);
-
-  if (!workspace) {
-    return NextResponse.redirect("/dashboard");
-  }
-
   const { bodyText, header, subject } = await getEmailTemplateForUser({
     waitlistID,
     template: "invite",
   });
+
   const {
     logoFileURL: websiteLogo,
     supportEmail,
@@ -61,18 +57,25 @@ export async function POST(request: NextRequest, context: { params: { waitlistID
   }
 
   switch (type) {
-    case "count":
-      return await handleCount();
-    case "list":
-      return await handleList();
+    case "count": {
+      const emailsList = await getInvitesListByCount(selectionMethod, inviteCount, waitlistID);
+      return await handleSend(emailsList);
+    }
+    case "list": {
+      const emailsList = invitesList;
+      return await handleSend(emailsList);
+    }
   }
 
-  async function handleCount() {
+  async function handleSend(
+    emailsList: {
+      email: string;
+      id: string;
+    }[],
+  ) {
     if (!workspace) {
       return NextResponse.redirect("/dashboard");
     }
-
-    const emailsList = await getInvitesListByCount(selectionMethod, inviteCount, waitlistID);
 
     const resendResponse =
       process.env.NODE_ENV === "development"
@@ -98,12 +101,10 @@ export async function POST(request: NextRequest, context: { params: { waitlistID
 
     if (!resendResponse.error) {
       if (resendResponse.data) {
-        console.log("Resend response", resendResponse.data);
         const emailIDs = resendResponse.data.data.map((email) => email.id);
         await createInvite(waitlistID, emailIDs, emailsList);
       }
     } else {
-      console.log("Error sending emails:", resendResponse.error);
       return NextResponse.json({ message: "error" });
     }
 
@@ -117,70 +118,9 @@ export async function POST(request: NextRequest, context: { params: { waitlistID
       if (!subId) {
         return NextResponse.json({ code: "NO_SUBSCRIPTION_FOUND", message: "error" });
       }
+
       const sub = await stripe.subscriptions.retrieve(subId);
-      console.log("recording usage for", sub.items.data[0].id);
-      await stripe.subscriptionItems.createUsageRecord(sub.items.data[0].id, {
-        quantity: inviteCount,
-        timestamp: "now",
-        action: "increment",
-      });
-    }
 
-    return NextResponse.json({ message: "success" });
-  }
-
-  async function handleList() {
-    if (!workspace) {
-      return NextResponse.redirect("/dashboard");
-    }
-
-    const emailsList = invitesList;
-
-    const resendResponse =
-      process.env.NODE_ENV === "development"
-        ? { data: { data: [{ id: "1" }, { id: "2" }] }, error: null }
-        : await resend.batch.send(
-            emailsList.map((email) => {
-              return {
-                from: "Ali B <onboarding@resend.dev>",
-                to: email.email,
-                subject: subject ?? "",
-                react: InviteTemplate({
-                  bodyText,
-                  header,
-                  websiteLogo,
-                  brandColor,
-                  supportEmail,
-                  websiteLink,
-                  websiteName,
-                }),
-              };
-            }),
-          );
-
-    if (!resendResponse.error) {
-      if (resendResponse.data) {
-        console.log("Resend response", resendResponse.data);
-        const emailIDs = resendResponse.data.data.map((email) => email.id);
-        await createInvite(waitlistID, emailIDs, emailsList);
-      }
-    } else {
-      console.log("Error sending emails:", resendResponse.error);
-      return NextResponse.json({ message: "error" });
-    }
-
-    await updateRemainingInvitesForWorkspace({
-      workspaceID: workspace.workspaceID,
-      remainingInvites: workspace.remainingInvites - inviteCount,
-    });
-
-    if (workspace?.stripeSubscriptionID) {
-      const subId = workspace?.stripeSubscriptionID;
-      if (!subId) {
-        return NextResponse.json({ code: "NO_SUBSCRIPTION_FOUND", message: "error" });
-      }
-      const sub = await stripe.subscriptions.retrieve(subId);
-      console.log("recording usage for", sub.items.data[0].id);
       await stripe.subscriptionItems.createUsageRecord(sub.items.data[0].id, {
         quantity: inviteCount,
         timestamp: "now",
