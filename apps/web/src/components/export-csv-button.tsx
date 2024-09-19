0e89cb979aec116de1fbade7c009a9c8c3a78745
Ali Bassiouni <aabassiouni@hotmail.com>
2024-09-19 04:09:50 +0300

    migrate export csv button

--- apps/web/src/components/export-csv-button.tsx
+++ apps/web/src/components/export-csv-button.tsx
@@ -1,30 +1,41 @@
 import { cn } from "@/lib/utils";
+import { useMutation } from "@tanstack/react-query";
 import { Loader2 } from "lucide-react";
 import { useParams } from "next/navigation";
-import React, { useState } from "react";
+import React from "react";
 import { Button } from "./ui/button";
 
+const useExportCsvMutation = () => {
+  return useMutation({
+    mutationFn: async (params: { waitlist: string }) => {
+      const res = await fetch("/api/export", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          waitlist: params.waitlist,
+        }),
+      });
+      return res.json();
+    },
+  });
+};
+
 function ExportToCsvButton({ className }: { className?: string }) {
-  const { waitlist } = useParams();
-  const [loading, setLoading] = useState(false);
+  const { waitlist } = useParams<{ waitlist: string }>();
 
-  async function handleClick() {
-    setLoading(true);
-    const res = await fetch("/api/export", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ waitlist }),
-    });
-
-    const _data = await res.json();
-    setLoading(false);
-  }
+  const { mutate: exportCsv, isPending } = useExportCsvMutation();
 
   return (
-    <Button disabled={loading} onClick={handleClick} className={cn("h-8 px-3 py-2", className)}>
-      {loading ? <Loader2 className="animate-spin" /> : "Export"}
+    <Button
+      disabled={isPending}
+      onClick={() => {
+        exportCsv({ waitlist });
+      }}
+      className={cn("h-8 px-3 py-2", className)}
+    >
+      {isPending ? <Loader2 className="animate-spin" /> : "Export"}
     </Button>
   );
 }
